562d98aed109cb576ed8d4e039a9b27605cea20b
Dmitry Ilyin <idv1985@gmail.com>
2019-02-28 21:25:09 +0300

    Update cache.go

--- pkg/carbon_registry/cache.go
+++ pkg/carbon_registry/cache.go
@@ -71,7 +71,7 @@ func (c *CarbonCache) Listen(channel syslog.LogPartsChannel) {
 		if messageLength >= 2 {
 			value, err = strconv.ParseFloat(messageFields[1], 64)
 			if err != nil {
-				log.Warnf("Could not parse value for metric: '%s' from: '%s' - %s", metric, source, err)
+				log.Warnf("Could not parse message: '%s' from: '%s' - %s", metric, source, err)
 				continue
 			}
 		}
@@ -79,7 +79,7 @@ func (c *CarbonCache) Listen(channel syslog.LogPartsChannel) {
 		if messageLength >= 3 {
 			timestamp, err = strconv.ParseUint(messageFields[2], 10, 64)
 			if err != nil {
-				log.Warnf("Could not parse timestamp for metric: '%s' with value: %f from: '%s' - %s", metric, value, source, err)
+				log.Warnf("Could not parse message: '%s' from: '%s' - %s", metric, source, err)
 				continue
 			}
 		}
